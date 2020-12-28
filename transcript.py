8d6866d7eed35729c6a9810dedcd6ad6305c54ab
Abel Yagubyan <43325291+Abelo9996@users.noreply.github.com>
2020-12-27 22:49:52 -0800

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -3,6 +3,9 @@ import json
 import requests
 import pdfplumber
 import json
+import img2pdf
+import urllib.request
+from PIL import Image
 def transcript_call(url1,req,ourclass=0):
   def download_file(url):
       local_filename = url.split('/')[-1]
@@ -12,8 +15,25 @@ def transcript_call(url1,req,ourclass=0):
           with open(local_filename, 'wb') as f:
               f.write(r.content)
       return local_filename
-  invoice = url1
-  invoice_pdf = download_file(invoice)
+  if url1[len(url1)-3:len(url1)] != "pdf":
+    invoice_pdf = "tstoutput.pdf"
+    if url1.split('.')[-1] == "JPG":
+      ourfile = "tstyes.JPG"
+      urllib.request.urlretrieve(url1, "tstyes.JPG")
+    else:
+      ourfile = "tstyes.PNG"
+      urllib.request.urlretrieve(url1, "tstyes.PNG")
+    image = Image.open(ourfile) 
+    pdf_bytes = img2pdf.convert(image.filename) 
+    file = open(invoice_pdf, "wb") 
+    file.write(pdf_bytes) 
+    image.close() 
+    file.close()
+    os.remove(ourfile)
+  if url1[len(url1)-3:len(url1)] == "pdf":
+    invoice = url1
+    invoice_pdf = download_file(invoice)
+  url1 = invoice_pdf 
   try:
     with pdfplumber.open(invoice_pdf) as pdf:
         os.remove(invoice_pdf)
@@ -24,29 +44,56 @@ def transcript_call(url1,req,ourclass=0):
         secondname = ""
         lvl = ""
         grade=""
-        if req == "grade":
+        for i in range(0,len(pdf.pages)):
+          page = pdf.pages[i]
+          text = page.extract_text(x_tolerance=2)
+          lines = text.split('\n')
           for line in lines:
-            if ourclass in line:
-              grade = line.split(' ')[-2]
+            if "My Academics" in line:
+              ourbool = True
+              break;
+            else:
+              ourbool = False
+        if req == "grade":
+          for i in range(0,len(pdf.pages)):
+            page = pdf.pages[i]
+            text = page.extract_text(x_tolerance=2)
+            lines = text.split('\n')
+            for line in lines:
+              if ourclass in line and ourbool == False:
+                grade = line.split(' ')[-2]
+              elif ourclass in line and ourbool:
+                grade = line.split(' ')[-1]
           if grade == "":
             return None
           else:
             return json.dumps({"grade":grade})
         elif req == "signup":
-          for line in lines:
-            if "Name " in line:
-              firstname = line.split(' ')[1]
-              lastname = line.split(' ')[-1]
-            if "Level " in line:
-              lvl = line.split(' ')[-1]
+          for i in range(0,len(pdf.pages)):
+            page = pdf.pages[i]
+            text = page.extract_text(x_tolerance=2)
+            lines = text.split('\n')
+            for line in lines:
+              if ourbool == False:
+                if "Name " in line:
+                  firstname = line.split(' ')[1]
+                  lastname = line.split(' ')[-1]
+                if "Level " in line:
+                  lvl = line.split(' ')[-1]
+              elif ourbool:
+                firstname = lines[3].split(' ')[0]
+                lastname = lines[3].split(' ')[-1]
+                lvl = [lines[x].split(' ')[-1] for x in range(0,len(lines)) if  "Level " in lines[x]]
+                break;
+            break;
           if firstname == "" or lastname=="" or lvl == "":
             return None
           else:
             return json.dumps({"firstname":firstname,"lastname":lastname,"lvl":lvl})
         else:
           return None
-  except:
-    return "ERROR"
+  except Exception as e:
+    return None
 
-x = transcript_call("http://www.work.fibonia.com/1/html/uploadstranscript/5fa91e4b8d63f3.16225769.pdf","grade","INDENG 290")
+x = transcript_call("https://work.fibonia.com/1/html/uploadstranscript/transcript.pdf","signup","INDENG 242")
 x
