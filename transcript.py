4151d6052ca7a9203f5f7e6a2d475dae0c8c6408
Abel Yagubyan <43325291+Abelo9996@users.noreply.github.com>
2020-12-31 01:54:26 -0800

    Update transcript.py

--- transcript.py
+++ transcript.py
@@ -7,6 +7,7 @@ import img2pdf
 import urllib.request
 from PIL import Image
 def transcript_call(url1,req,ourclass=0):
+  found = False
   def download_file(url):
       local_filename = url.split('/')[-1]
       headers = {'User-Agent': 'XY', 'Content-type': 'application/json'}
@@ -49,8 +50,11 @@ def transcript_call(url1,req,ourclass=0):
           text = page.extract_text(x_tolerance=2)
           lines = text.split('\n')
           for line in lines:
-            if "My Academics" in line:
-              ourbool = True
+            if "Academic Summary" in line:
+              ourbool = "AS"
+              break;
+            elif "My Academics" in line:
+              ourbool = "MA"
               break;
             else:
               ourbool = False
@@ -60,9 +64,16 @@ def transcript_call(url1,req,ourclass=0):
             text = page.extract_text(x_tolerance=2)
             lines = text.split('\n')
             for line in lines:
-              if ourclass in line and ourbool == False:
+              x = line.split('   ')
+              x = ' '.join(line.split())
+              if ourclass in x and ourbool == False:
+                val = x.split()
+                for item in val:
+                  if len(item) < 3 and val.index(item) != 1 and item != "&":
+                    grade = item
+              elif ourclass in line and ourbool == "AS":
                 grade = line.split(' ')[-2]
-              elif ourclass in line and ourbool:
+              elif ourclass in line and ourbool == "MA":
                 grade = line.split(' ')[-1]
           if grade == "":
             return None
@@ -75,25 +86,31 @@ def transcript_call(url1,req,ourclass=0):
             lines = text.split('\n')
             for line in lines:
               if ourbool == False:
-                if "Name " in line:
-                  firstname = line.split(' ')[1]
-                  lastname = line.split(' ')[-1]
-                if "Level " in line:
-                  lvl = line.split(' ')[-1]
-              elif ourbool:
+                if "Name:" in line:
+                  name = line.split()[1]
+                  firstname = name.partition(',')[-1]
+                  lastname = name.partition(',')[0]
+                  lvl = "N"
+              elif ourbool == "AS":
                 firstname = lines[3].split(' ')[0]
                 lastname = lines[3].split(' ')[-1]
                 lvl = [lines[x].split(' ')[-1] for x in range(0,len(lines)) if  "Level " in lines[x]]
                 break;
-            break;
+              elif ourbool == "MA":
+                firstname = lines[3].split(' ')[0]
+                lastname = lines[3].split(' ')[-1]
+                lvl = [lines[x].split(' ')[-1] for x in range(0,len(lines)) if  "Level " in lines[x]]
+                found = True
+                break;
+            if found == True:
+              break;
           if firstname == "" or lastname=="" or lvl == "":
             return None
           else:
-            return json.dumps({"firstname":firstname,"lastname":lastname,"lvl":lvl})
+            return json.dumps({"firstname":firstname,"lastname":lastname,"lvl":lvl[0]})
         else:
           return None
   except Exception as e:
     return None
 
-x = transcript_call("https://work.fibonia.com/1/html/uploadstranscript/transcript.pdf","signup","INDENG 242")
-x
+x = transcript_call("https://work.fibonia.com/1/html/uploadstranscript/transcript2.pdf","grade","INDENG 242")
